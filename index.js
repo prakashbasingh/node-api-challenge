9e453e99a91852547d3b4bb601f3ec892fe3b593
unknown <prakashbahadursingh2@gmail.com>
2020-07-10 12:36:30 -0700

    typo found and corrected while checking deployment issue

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 
 const server = require("./server.js")
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`\n***** Server is Running on http://localhost:${PORT} *****\n`);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n***** Server is Running on http://localhost:${port} *****\n`);
 });
