8d450f98e44697d0119df6606451f45ce092bdf8
unknown <prakashbahadursingh2@gmail.com>
2020-07-10 11:14:19 -0700

    dotenv dependency installed and port configured to new prot 5555

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require("dotenv").config();
+
+
 const server = require("./server.js")
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`\n***** Server is Running on http://localhost:${PORT} *****\n`);
 });
